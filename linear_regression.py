a04749035e489ad6c5d3ccf97366821d7be1157e
kevinfir <95353256+kevinfir@users.noreply.github.com>
2024-10-05 17:05:23 +0800

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -6,7 +6,8 @@ from sklearn.linear_model import LinearRegression
 st.title("Linear Regression")
 st.header("What is Linear Regression?什麼是線性迴歸?")
 st.text("還記得函數(function)的形式: y = mx + b或y= ax + b")
-st.text("這都不重要，重要的是本身是用一個已知的值推估未知的值")
+st.text("這都不重要")
+st.text("重要的是本身是用一個已知的值推估未知的值")
 st.text("神奇的來了!怎麼這樣可以預測呢?")
 st.text("這就是機器學習的魔法啊!")
 st.text("這關乎到數學裡的統計學")
@@ -14,34 +15,39 @@ st.text("我們把它繪製成以下的圖表就一目了然了")
 
 def get_data():
     data = pd.read_excel("regression_data.xlsx")
-    data = data.reset_index(drop=True)
     return data
-data = get_data()
+
 def sidebar_func(data):
-    weight = data["體重(公斤)"]
-    binding = data["坐姿體前彎(公分)"]
-    jumping = data["立定跳遠(公分)"]
-    situps = data["仰臥起坐(下)"]
+    weight = data["weight(kg)"]
+    binding = data["Seated Forward Bend(cm)"]
+    jumping = data["Standing long jump(cm)"]
+    situps = data["sit-up(once)"]
     with st.sidebar:
         st.header("篩選資料")
         st.write("選擇要預測的資料")
         selection = st.selectbox("選擇資料", ["體重(公斤)", "坐姿體前彎(公分)", "立定跳遠(公分)", "仰臥起坐(下)"])
     if selection == "體重(公斤)":
+        weight = pd.DataFrame(weight)
         return weight
     elif selection == "坐姿體前彎(公分)":
+        binding = pd.DataFrame(binding)
         return binding
     elif selection == "立定跳遠(公分)":
+        jumping = pd.DataFrame(jumping)
         return jumping
     else:  
+        situps = pd.DataFrame(situps)
         return situps
+data = get_data()
+value = sidebar_func(data)
 
-st.table(data)
 
-y = sidebar_func(data)
 
-x= data["身高(公分)"]
+
+
+x= data["height(cm)"]
 x = pd.to_numeric(x)
-y = pd.to_numeric(y)
+y = pd.to_numeric(value)
 x = x.values.reshape(-1, 1)
 model = LinearRegression()
 model.fit(x, y)
@@ -52,8 +58,8 @@ x_range = np.linspace(x.min(), x.max(), 100).reshape(-1, 1)
 y_pred = model.predict(x_range)
 ax.plot(x_range, y_pred, color='red', label=f'Regression line: y = {slope:.2f}x + {intercept:.2f}')
 plt.scatter(x, y, color='blue', label='Data points')
-ax.set_xlabel("身高(公分)")
-ax.set_ylabel("体重(公斤)")
+ax.set_xlabel("height(cm)")
+ax.set_ylabel(value.columns[0])
 ax.set_title('Simple Linear Regression with scikit-learn')
 ax.legend()
 
