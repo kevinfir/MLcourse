90c51d84d859a4bf829192027389d30a6b8bb5f6
kevinfir <95353256+kevinfir@users.noreply.github.com>
2024-10-06 21:02:01 +0800

    Update linear regression.py

--- linear regression.py
+++ linear regression.py
@@ -5,12 +5,17 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 st.title("Linear Regression")
 st.header("What is Linear Regression?什麼是線性迴歸?")
-st.text("還記得函數(function)的形式: y = mx + b或y= ax + b")
+st.text("還記得函數(function)的形式")
+st.text("y = mx + b或y= ax + b")
 st.text("這都不重要")
 st.text("重要的是本身是用一個已知的值推估未知的值")
 st.text("神奇的來了!怎麼這樣可以預測呢?")
 st.text("這就是機器學習的魔法啊!")
 st.text("這關乎到數學裡的統計學")
+st.text("線性迴歸就是一種統計學的方法")
+st.text("假設某一個值增加另外一個值也會增加")
+st.text("分析兩者的關係，假設關係成立")
+st.text("我們就會認為它呈現互相增長的關係")
 st.text("我們把它繪製成以下的圖表就一目了然了")
 
 def get_data():
@@ -27,18 +32,19 @@ def sidebar_func(data):
         st.write("選擇要預測的資料")
         selection = st.selectbox("選擇資料", ["體重(公斤)", "坐姿體前彎(公分)", "立定跳遠(公分)", "仰臥起坐(下)"])
     if selection == "體重(公斤)":
-        weight = pd.DataFrame(weight)
+        
         return weight
     elif selection == "坐姿體前彎(公分)":
-        binding = pd.DataFrame(binding)
+       
         return binding
     elif selection == "立定跳遠(公分)":
-        jumping = pd.DataFrame(jumping)
+        
         return jumping
     else:  
-        situps = pd.DataFrame(situps)
+        
         return situps
 data = get_data()
+st.table(data)
 value = sidebar_func(data)
 
 
@@ -46,8 +52,7 @@ value = sidebar_func(data)
 
 
 x= data["height(cm)"]
-x = pd.to_numeric(x)
-y = pd.to_numeric(value)
+y = value
 x = x.values.reshape(-1, 1)
 model = LinearRegression()
 model.fit(x, y)
@@ -59,6 +64,7 @@ y_pred = model.predict(x_range)
 ax.plot(x_range, y_pred, color='red', label=f'Regression line: y = {slope:.2f}x + {intercept:.2f}')
 plt.scatter(x, y, color='blue', label='Data points')
 ax.set_xlabel("height(cm)")
+value = pd.DataFrame(value)
 ax.set_ylabel(value.columns[0])
 ax.set_title('Simple Linear Regression with scikit-learn')
 ax.legend()
